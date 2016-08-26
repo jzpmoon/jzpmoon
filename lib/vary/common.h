34ae4c885ace1f3bd505ad3b8713b04af509b83e
root <root@bogon.(none)>
2016-08-27 07:47:34 +0800

    modify

--- lib/vary/common.h
+++
@@ -1,7 +0,0 @@
-#ifndef VR_COMMON_H
-#define VR_COMMON_H
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <assert.h>
-#endif
--- lib/vary/trcomn.h
+++
@@ -1,4 +0,0 @@
-#ifndef VR_TRCOMN_H
-#define VR_TRCOMN_H
-#include "common.h"
-#endif
