87a18b19e2bef032d99c893b7b0292ba9eb82631
jzpmoon <2481957497@qq.com>
2016-09-05 10:50:37 +0800

    rm node.h node.c type.h in varia

--- varia/node.h
+++
@@ -1,6 +0,0 @@
-#ifndef VR_NODE_H
-#define VR_NODE_H
-
-void vr_nd_ptr_set(void* nd_ptr,void* ptr,int nd_ptr_sz,int ptr_sz);
-
-#endif
--- varia/type.h
+++
@@ -1,6 +0,0 @@
-#ifndef VR_TYPE_H
-#define VR_TYPE_H
-
-typedef int (*compare)(void* max,void* min,int mxsz,int mnsz);
-
-#endif
